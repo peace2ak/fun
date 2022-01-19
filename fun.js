c2a4d0044b3b8c7b08466f5a255db36dfb3f7776
unknown <fear9808@gmail.com>
2022-01-19 20:09:47 +0500

    nn

---
+++ fun.js
@@ -0,0 +1,14 @@
+// click();
+
+function click() {
+    let count = 0;
+    let i = 0;
+    while (count < 11) {
+        open('https://www.youtube.com/');
+        count++;
+
+        console.log(i,'running');
+        i++;
+    }
+}
+
